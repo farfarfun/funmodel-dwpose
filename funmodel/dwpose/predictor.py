66c83430907fafa34518dcd0b8d95fddaee4af4c
farfun <1007530194@qq.com>
2024-03-21 13:39:25 +0800

    add

--- funmodel/dwpose/predictor.py
+++ funmodel/dwpose/predictor.py
@@ -36,25 +36,24 @@ class DWposePredict(ImagePredictModel):
     def predict(self, ori_img, draw=False, *args, **kwargs):
         ori_img = ori_img.copy()
         H, W, C = ori_img.shape
-        with torch.no_grad():
-            candidate, subset = self.pose_estimation(ori_img)
-            candidate[..., 0] /= float(W)
-            candidate[..., 1] /= float(H)
+        candidate, subset = self.pose_estimation(ori_img)
+        candidate[..., 0] /= float(W)
+        candidate[..., 1] /= float(H)
 
-            un_visible = subset < 0.3
-            candidate[un_visible] = -1
+        un_visible = subset < 0.3
+        candidate[un_visible] = -1
 
-            pose = dict(
-                height=H,
-                width=W,
-                bodies=candidate[:, :18],
-                hand1=candidate[:, 92:113],
-                hand2=candidate[:, 113:134],
-                faces=candidate[:, 24:92],
-                foots=candidate[:, 18:24],
-                nums=candidate.shape[0],
-            )
+        pose = dict(
+            height=H,
+            width=W,
+            bodies=candidate[:, :18],
+            hand1=candidate[:, 92:113],
+            hand2=candidate[:, 113:134],
+            faces=candidate[:, 24:92],
+            foots=candidate[:, 18:24],
+            nums=candidate.shape[0],
+        )
 
-            if draw:
-                ori_img = self.draw_image(image=ori_img, result=pose)
-            return pose, ori_img
+        if draw:
+            ori_img = self.draw_image(image=ori_img, result=pose)
+        return pose, ori_img
